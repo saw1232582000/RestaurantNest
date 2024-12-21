c519a1f1f341c069c93f34283f9d6f144f4cd819
saw1232582000 <doomtakersaw@gmail.com>
2024-12-21 21:36:35 +0700

    fix order list status filter bug

--- src/core/domain/order/dto/OrderFilter.ts
+++ src/core/domain/order/dto/OrderFilter.ts
@@ -1,14 +1,15 @@
+import { Nullable } from "@src/core/common/type/CommonTypes";
 import { Status } from "@src/core/common/type/StatusEnum";
 import { BaseFilterSchema } from "src/core/common/schema/BaseFilterSchema";
 
 export class OrderFilter extends BaseFilterSchema{
     startDate:string;
     endDate:string;
-    status:Status;
+    status:Nullable<Status>;
     constructor(startDate:string,endDate:string,take:number,skip:number,status:Status){
         super(take,skip)
         this.startDate=startDate
         this.endDate=endDate
-        this.status=status || Status.PROCESSING
+        this.status=status
     }
 }
